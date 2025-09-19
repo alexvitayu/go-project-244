baea56812169ced330cd6caacd49544317d07a01
Aliaksei Yuryeu <alexvitayu1212@gmail.com>
2025-09-19 11:03:34 +0300

    Add fixtures for test

--- src/gendiff_test.go
+++ src/gendiff_test.go
@@ -1,33 +1,52 @@
 package code
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-var testCases = []struct {
-	name   string
-	data1  map[string]interface{}
-	data2  map[string]interface{}
-	expect string
-}{
-	{name: "test1",
-		data1: map[string]interface{}{
-			"host":    "hexlet.io",
-			"timeout": 50,
-			"proxy":   "123.234.53.22",
-			"follow":  false,
-		},
-		data2: map[string]interface{}{
-			"timeout": 20,
-			"verbose": true,
-			"host":    "hexlet.io",
-		},
-		expect: " - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true",
-	},
+// мой тест с использованием фикстур
+func TestGenDiffWithFixtures(t *testing.T) {
+	path1 := filepath.Join("../testdata/file1.json") //фикстура №1
+	path2 := filepath.Join("../testdata/file2.json") //фикстура №2
+	path3 := filepath.Join("../testdata/result.txt") //фикстура №3
+
+	str, err := GenDiff(path1, path2, "stylish")
+	require.NoError(t, err) // если произошла ошибка в основном коде, то и нет смысла двигаться дальше
+
+	bytes, err := os.ReadFile(path3)
+	require.NoError(t, err)             // если произошла ошибка чтения, то нет смысла идти дальше
+	assert.Equal(t, string(bytes), str) // утверждаем, что ожидаемая строка равна полученной строке
 }
 
+// мой тест до использовния фикстур
 func TestGenDiff(t *testing.T) {
+	var testCases = []struct {
+		name   string
+		data1  map[string]interface{}
+		data2  map[string]interface{}
+		expect string
+	}{
+		{name: "test1",
+			data1: map[string]interface{}{
+				"host":    "hexlet.io",
+				"timeout": 50,
+				"proxy":   "123.234.53.22",
+				"follow":  false,
+			},
+			data2: map[string]interface{}{
+				"timeout": 20,
+				"verbose": true,
+				"host":    "hexlet.io",
+			},
+			expect: " - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true",
+		},
+	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			got := genDiff(tc.data1, tc.data2)
